7829a3ddf2560b951ab85f14dacb1b6ecdac3e4a
Andrew Olson <andruolson@gmail.com>
2026-02-24 00:31:52 -0700

    Did the exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -2,9 +2,29 @@
 {
     public class Program
     {
+
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+            
+            int sum = a + b;
+            int difference = a - b;
+            int product = a * b;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}.");
+            
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            double area = AreaOfCircle(radius);
+            Console.WriteLine($"The area of the circle is {area}.");
         }
+        
     }
 }
